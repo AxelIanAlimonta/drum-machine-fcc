0f19fa2be2548599b56544ee210e509cb01916f3
Axel Ian Alimonta <axelalimonta@gmail.com>
2023-11-24 20:26:49 -0300

    Los sonidos no estaba funcionando en el despliegue de la aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
 import "./styles/reset.css";
-import { canciones } from "./js/canciones";
 import ToggleSwitch from "./components/ToggleSwitch";
 import DrumPad from "./components/DrumPad";
 import { useEffect } from "react";
 import { useState } from "react";
+
+import Heater1 from "./audio/Heater1.mp3";
+import Heater2 from "./audio/Heater2.mp3";
+import Heater3 from "./audio/Heater3.mp3";
+import Heater4 from "./audio/Heater4.mp3";
+import Clap from "./audio/Clap.mp3";
+import OpenHH from "./audio/Open-HH.mp3";
+import KicknHat from "./audio/Kick-n'-Hat.mp3";
+import Kick from "./audio/Kick.mp3";
+import ClosedHH from "./audio/Closed-HH.mp3";
 
 function App() {
   const [txtDisplay, setTxtDisplay] = useState("");
@@ -24,22 +33,17 @@ function App() {
 
   return (
     <div id="drum-machine">
-      <audio src="./audio/Heater1.mp3" id="asd"></audio>
+      <audio src="./audio/Heater1.mp3"></audio>
       <div className="padContainer">
-        <DrumPad
-          src={canciones.Q.src}
-          idBtn="Q"
-          idCancion={canciones.Q.id}
-          src2="./src/audio/aaa.png"
-        />
-        <DrumPad src={canciones.W.src} idBtn="W" idCancion={canciones.W.id} />
-        <DrumPad src={canciones.E.src} idBtn="E" idCancion={canciones.E.id} />
-        <DrumPad src={canciones.A.src} idBtn="A" idCancion={canciones.A.id} />
-        <DrumPad src={canciones.S.src} idBtn="S" idCancion={canciones.S.id} />
-        <DrumPad src={canciones.D.src} idBtn="D" idCancion={canciones.D.id} />
-        <DrumPad src={canciones.Z.src} idBtn="Z" idCancion={canciones.Z.id} />
-        <DrumPad src={canciones.X.src} idBtn="X" idCancion={canciones.X.id} />
-        <DrumPad src={canciones.C.src} idBtn="C" idCancion={canciones.C.id} />
+        <DrumPad src={Heater1} idBtn="Q" idCancion={"Heater1"} />
+        <DrumPad src={Heater2} idBtn="W" idCancion={"Heater2"} />
+        <DrumPad src={Heater3} idBtn="E" idCancion={"Heater3"} />
+        <DrumPad src={Heater4} idBtn="A" idCancion={"Heater4"} />
+        <DrumPad src={Clap} idBtn="S" idCancion={"Clap"} />
+        <DrumPad src={OpenHH} idBtn="D" idCancion={"Open-HH"} />
+        <DrumPad src={KicknHat} idBtn="Z" idCancion={"Kick-n'-Hat"} />
+        <DrumPad src={Kick} idBtn="X" idCancion={"Kick"} />
+        <DrumPad src={ClosedHH} idBtn="C" idCancion={"Closed-HH"} />
       </div>
       <div className="options">
         <div id="display" className="options-display">
